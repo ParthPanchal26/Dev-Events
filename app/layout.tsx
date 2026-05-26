99e84764e7dc44b9fb33272064ee3bc220547b1d
Parth Panchal <parth.code2625@gmail.com>
2026-05-26 14:33:45 +0530

    implement api routes, server actions, caching and new light rays, and the event details page

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,8 +32,18 @@ export default function RootLayout({
 
         <Navbar />
 
-        <div className="absolute inset-0 top-0 z-[-1] min-h-screen ">
-          <LightRays />
+        <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+          <LightRays
+            raysOrigin="top-center-offset"
+            raysColor="#5dfeca"
+            raysSpeed={0.5}
+            lightSpread={0.9}
+            rayLength={1.4}
+            followMouse={true}
+            mouseInfluence={0.02}
+            noiseAmount={0.0}
+            distortion={0.01}
+          />
         </div>
 
         <main>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import EventCard from '@/components/EventCard'
 import ExploreBtn from '@/components/ExploreBtn'
-import { events } from '@/lib/constants'
+import { IEvent } from '@/database';
+import { cacheLife } from 'next/cache';
+// import { events } from '@/lib/constants'
 
 
 // const events = [
@@ -14,7 +16,16 @@ import { events } from '@/lib/constants'
 // 	},
 // ]
 
-const Home = () => {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URI
+
+const Home = async () => {
+
+	'use cache';
+	cacheLife('hours')
+
+	const response = await fetch(`${BASE_URL}/api/events`);
+	const { events } = await response.json();
+
 	return (
 		<section>
 			<h1 className='text-center'>The Hub for Every Dev <br /> Event You Can&apos;t  Miss.</h1>
@@ -26,8 +37,10 @@ const Home = () => {
 				<h3>Featured Events</h3>
 
 				<ul className='events'>
-					{events.map((event) => (
-						<EventCard key={event.title} {...event} />
+					{events && events.length > 0 && events.map((event: IEvent) => (
+						<li key={event.title} className='list-none'>
+							<EventCard key={event.title} {...event} />
+						</li>
 					))}
 				</ul>
 			</div>
